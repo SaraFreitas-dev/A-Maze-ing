cc40a8d3ea403a407d031a9ee3d17f5cb5304645
Sara Fontes <sfontes94@gmail.com>
2026-04-24 16:46:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+
+```bash
+
+a-maze-ing # main folder
+|-a_maze_ing.py # main, entrypoint
+|-Makefile
+|-README.md
+|-seed_logs.txt
+|-requirements.txt # libs for venv
+|-pyproject.toml # project configuration (Python version, flake8 rules, authors, etc.)
+|
+|__config # folder
+|  |__config.txt # KEY=VALUE (WIDTH, HEIGHT, ENTRY, etc.) values
+|
+|__render # folder
+|  |__ __init__.py
+|  |__ascii_renderer.py # visual design ascii
+|  |__mlx_renderer.py # visual design mlx
+|  |__ menu_design.py # visual design of the main menu
+|  |__player_commands.py # make the maze playable
+|
+|__assets # folder - mlx
+|   |__textures
+|   |__fonts
+|   |__42_pattern.txt # 42 in ascii design
+|__utils # folder
+|
+|  |__ __init__.py
+|  |__ directions.py # N E S W (helpers)
+|  |__ errors.py # for error handling
+|
+|__tests # folder
+|  |__ __init__.py
+|  |__test_maze.py  # scripts for tests only
+|  |__test_solver.py # scripts for tests only
+|
+|__core # folder
+|  |__maze.py # maze structure (grid + cells)
+|  |__generator.py # algorithm to generate maze - Class MazeGenerator
+|  |__solver.py # BFS algorithm - shortest path to solve the maze
+|  |__bonus_solver.py # another algorithm to solve the maze
+|  |__exporter.py # convert to hex
+|
+|__ parsing # folder
+|  |__ __init__.py
+|  |__ config_parser.py # parse KEY=VALUE config file (WIDTH, HEIGHT, etc.)
+
+```
+ 
---
+++ parsing/config_parser.py
@@ -0,0 +1,84 @@
+import sys
+from typing import Any
+
+
+class ConfigError(Exception):
+    """
+    Custom exception for config
+    """
+    pass
+
+
+def get_config_path() -> str:
+    """
+    Get the config file_name
+    If no configuration is given as an argument,
+    Use the default_config.txt as default
+    """
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    return "config/default_config.txt"
+
+
+def check_mandatory_keys(config_keys: dict[str, Any]) -> None:
+    """
+    Check if all the mandatory keys are present and uncommented
+    From the config file
+    If not: raise an error - The maze can't be generated
+    """
+    required_k = {
+        "WIDTH",
+        "HEIGHT",
+        "ENTRY",
+        "EXIT",
+        "OUTPUT_FILE",
+        "PERFECT",
+    } 
+
+    missing = required_k - config_keys.keys()
+
+    if missing:
+        raise ConfigError(
+            f"Missing required key(s): {', '.join(missing)}"
+        )
+
+
+def parse_config(file_path: str) -> dict[str, Any]:
+    """
+    Read and transform the values of config.txt into a dict
+    """
+    config: dict[str, Any] = {}
+
+    try:
+        with open(file_path, "r") as file:
+            for i, line in enumerate(file, start=1):
+                line = line.strip()
+
+                # Ignore empty lines and comments
+                if not line or line.startswith('#'):
+                    continue
+
+                if "=" not in line:
+                    raise ConfigError(f"Invalid format on line {i}.")
+
+                key, value = line.split("=", 1)  # maxsplit=1
+                key = key.strip()  # Remove extra spaces
+                value = value.strip()
+
+                if key in config:
+                    raise ConfigError(f"Duplicate key '{key}' on line {i}.")
+
+                config[key] = value
+        check_mandatory_keys(config)
+        return config
+
+    except FileNotFoundError:
+        raise ConfigError(f"File '{file_path}' was not found.")
+
+
+if __name__ == "__main__":
+    try:
+        config = parse_config(get_config_path())
+        print(config)
+    except ConfigError as e:
+        print(e)
